64d2a3c72a296e3e61b2d1833b16c56bd0a9f090
minhhquann88 <dauminhquan882003@gmail.com>
2026-05-04 09:59:25 +0700

    first commit

---
+++ HelloWorlds/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorlds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            
+        }
+    }
+}
